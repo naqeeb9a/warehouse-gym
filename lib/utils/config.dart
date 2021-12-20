f6dad84005486f7b1abfe8f7cb890a8f5a3b8059
InTECHT <88443508+InTECHT@users.noreply.github.com>
2021-12-20 11:43:12 +0500

    Hassan ~ Add Basic Widgets

---
+++ lib/utils/config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const myWhite = Color(0xffffffff);
+const myBlack = Color(0xff0b0b0b);
+const myYellow = Color(0xfffbda2a);
+const noColor = Colors.transparent;
+
+bool obscureText = true;
---
+++ lib/widgets/text_widge.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:warehouse_gym/utils/dynamic_sizes.dart';
+
+Widget text(context, text, size, color, {bold = false}) {
+  return Text(
+    text,
+    style: TextStyle(
+      color: color,
+      fontSize: dynamicWidth(context, size),
+      fontWeight: bold == true ? FontWeight.bold : FontWeight.normal,
+    ),
+  );
+}
